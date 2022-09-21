b64b49c5c88b6c96135815a374a029258cdf262b
Pofigistsem <pofigistsem@gmail.com>
2022-09-22 01:10:05 +0200

    finished project

---
+++ client/src/components/ProjectCard.jsx
@@ -0,0 +1,18 @@
+
+export const ProjectCard = ({project}) => {
+    return (
+        <div className="col-md-6">
+            <div className="card mb-3">
+                <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h5 className="card-titile">{project.name}</h5>
+                        <a className="btn btn-light" href={`/projects/${project.id}`}>View</a>
+                    </div>
+                </div>
+                <p className="small">
+                    Status: <strong>{project.status}</strong>
+                </p>
+            </div>
+        </div>
+    );
+}
